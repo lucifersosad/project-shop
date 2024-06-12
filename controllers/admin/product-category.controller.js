13dabdf1841d25d0027a2144a42796e1c2d982e1
Lucifer <luciferxhnh@gmail.com>
2024-06-12 09:58:09 +0700

    chỉnh sửa danh mục sản phẩm

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,6 +1,6 @@
 const ProductCategory = require("../../models/product-category.model");
 const systemConfig = require("../../config/system");
-const createTreeHelper = require("../../helpers/createTree")
+const createTreeHelper = require("../../helpers/createTree");
 
 // [GET] /admin/products-category
 module.exports.index = async (req, res) => {
@@ -26,7 +26,7 @@ module.exports.create = async (req, res) => {
 
   const records = await ProductCategory.find(find);
 
-  const newRecords = createTreeHelper(records)
+  const newRecords = createTreeHelper(records);
 
   res.render("admin/pages/products-category/create", {
     pageTitle: "Tạo danh mục sản phẩm",
@@ -47,4 +47,38 @@ module.exports.createPost = async (req, res) => {
   await record.save();
 
   res.redirect(`${systemConfig.prefixAdmin}/products-category`);
+};
+
+// [GET] /admin/products-category/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const data = await ProductCategory.findOne({ _id: id, deleted: false });
+
+    const records = await ProductCategory.find({
+      deleted: false,
+    });
+
+    const newRecords = createTreeHelper(records);
+
+    res.render("admin/pages/products-category/edit", {
+      pageTitle: "Chỉnh sửa danh mục sản phẩm",
+      data: data,
+      records: newRecords,
+    });
+  } catch (error) {
+    res.redirect(`${systemConfig.prefixAdmin}/products-category`);
+  }
+};
+
+// [PATCH] /admin/products-category/edit/:id
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+
+  req.body.position = parseInt(req.body.position);
+
+  await ProductCategory.updateOne({ _id: id }, req.body);
+
+  res.redirect("back");
 };
