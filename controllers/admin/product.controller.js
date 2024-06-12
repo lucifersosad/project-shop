5fbbec9ec9cbbffc96c16d13977cd98a9012e32b
Lucifer <luciferxhnh@gmail.com>
2024-06-12 10:38:25 +0700

    chỉnh sửa sp theo danh mục

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -175,9 +175,16 @@ module.exports.edit = async (req, res) => {
 
     const product = await Product.findOne(find);
 
+    const category = await ProductCategory.find({
+      deleted: false,
+    });
+
+    const newCategory = createTreeHelper(category);
+
     res.render("admin/pages/products/edit", {
       pageTitle: "Chỉnh sửa sản phẩm",
       product: product,
+      category: newCategory,
     });
   } catch (error) {
     res.redirect(`${systemConfig.prefixAdmin}/products`);
