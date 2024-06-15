82c4b86ad505c85b022038ba8eafee9558d8f809
Lucifer <luciferxhnh@gmail.com>
2024-06-15 09:44:49 +0700

    cart header

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -6,8 +6,21 @@ module.exports.cartId = async (req, res, next) => {
 
     const expiresTime = 1000 * 60 * 60 * 24 * 365;
 
-    res.cookie("cartId", cart.id, { expires: new Date(Date.now() + expiresTime) });
+    res.cookie("cartId", cart.id, {
+      expires: new Date(Date.now() + expiresTime),
+    });
   } else {
+    const cart = await Cart.findOne({
+      _id: req.cookies.cartId,
+    });
+
+    cart.totalQuantity = cart.products.reduce(
+      (sum, item) => sum + item.quantity,
+      0
+    );
+
+    res.locals.miniCart = cart;
   }
+
   next();
 };
